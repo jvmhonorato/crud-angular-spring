a2e3d9b43f56dde00c4999faefa77c19e1382303
vituhonorato <vituhonorato.89@gmail.com>
2022-12-21 17:24:38 -0300

    Search by ID

--- crud-spring/src/main/java/com/honorato/crudspring/controler/CourseController.java
+++ crud-spring/src/main/java/com/honorato/crudspring/controler/CourseController.java
@@ -2,10 +2,12 @@ package com.honorato.crudspring.controler;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,14 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository
+        .findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
     }
     
     //@ResquestMapping(method = ResquestMethod.GET)
