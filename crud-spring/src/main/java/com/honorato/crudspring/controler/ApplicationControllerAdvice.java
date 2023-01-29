4cc14dc9d9052abcecd1ba8198abb6dbda25abee
vituhonorato <vituhonorato.89@gmail.com>
2023-01-29 19:29:04 -0300

    add notation @ResponseStatus(HttpStatus) in ApplicationControllerAdvice.java

--- crud-spring/src/main/java/com/honorato/crudspring/controler/ApplicationControllerAdvice.java
+++ crud-spring/src/main/java/com/honorato/crudspring/controler/ApplicationControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.honorato.crudspring.controler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.honorato.crudspring.exception.RecordNotFoundExeption;
@@ -9,6 +11,7 @@ import com.honorato.crudspring.exception.RecordNotFoundExeption;
 public class ApplicationControllerAdvice {
 
     @ExceptionHandler(RecordNotFoundExeption.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNotFoundException(RecordNotFoundExeption ex){
         return  ex.getMessage();
     };
