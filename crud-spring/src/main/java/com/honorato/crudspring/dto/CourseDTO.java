174fdea45a119d200b0a9f828c1ae9b96ce33716
vituhonorato <vituhonorato.89@gmail.com>
2023-01-30 10:42:08 -0300

    add DTO/CourseDTO.java using record method

--- crud-spring/src/main/java/com/honorato/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/honorato/crudspring/dto/CourseDTO.java
@@ -2,33 +2,16 @@ package com.honorato.crudspring.dto;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
 
-@Data
-public class CourseDTO {
-    
-    
-    private Long _id;
-
-    @NotBlank
-    @NotNull 
-    @Length(min =5,max = 100)
-    
-    private String name;
-
-    @NotNull
-    @Length(max = 10)
-    @Pattern(regexp = "back-end|front-end")
+public record CourseDTO(
+    @JsonProperty("_id") Long id, 
+    @NotBlank @NotNull @Length(min =5,max = 100) String name, 
+    @NotNull @Length(max = 10) @Pattern(regexp = "back-end|front-end") String category
+     ) {
    
-    private String category;
-
-    @NotNull
-    @Length(max = 10)
-    @Pattern(regexp = "Active|Inactive")
-    
-    private String status = "Active"; 
 }
