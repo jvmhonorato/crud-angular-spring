3aad469a605bb312c0df6e0fea3c1b77c35229e5
vituhonorato <vituhonorato.89@gmail.com>
2023-07-05 22:33:04 -0300

    add CourseMapper class

--- crud-spring/src/main/java/com/honorato/crudspring/dto/mapper/CourseMapper.java
+++ crud-spring/src/main/java/com/honorato/crudspring/dto/mapper/CourseMapper.java
@@ -9,6 +9,25 @@ import com.honorato.crudspring.model.Course;
 
 public class CourseMapper {
     public CourseDTO toDTO(Course course) {
+        if (course == null) {
+            return null;
+        }
         return new CourseDTO(course.getId(), course.getName(), course.getCategory());
+    }
+
+    public Course toEntity(CourseDTO courseDTO) {
+        if (courseDTO == null) {
+            return null;
+        }
+        Course course = new Course();
+        if (courseDTO.id() != null){
+            course.setId(courseDTO.id());
+        }
+        course.setName(courseDTO.name());
+        course.setCategory(courseDTO.category());
+        course.setStatus("Ativo");
+        return course;
+
+        //builder pattern
     }
 }
