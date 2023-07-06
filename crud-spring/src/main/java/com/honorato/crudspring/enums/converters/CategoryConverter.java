a106ff81125af91da8c20fa8c1c1f01afed92847
vituhonorato <vituhonorato.89@gmail.com>
2023-07-06 18:27:08 -0300

    add converter using enums

--- crud-spring/src/main/java/com/honorato/crudspring/enums/converters/CategoryConverter.java
+++ crud-spring/src/main/java/com/honorato/crudspring/enums/converters/CategoryConverter.java
@@ -1,21 +1,29 @@
 package com.honorato.crudspring.enums.converters;
-
+import java.util.stream.Stream;
 import com.honorato.crudspring.enums.Category;
 
 import jakarta.persistence.AttributeConverter;
-
+import jakarta.persistence.Converter;
+@Converter(autoApply = true)
 public class CategoryConverter implements AttributeConverter<Category, String> {
 
     @Override
-    public String convertToDatabaseColumn(Category attribute) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToDatabaseColumn'");
+    public String convertToDatabaseColumn(Category category) {
+        if(category == null){
+           return null;
+        }
+        return category.getValue();
     }
 
     @Override
-    public Category convertToEntityAttribute(String dbData) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToEntityAttribute'");
+    public Category convertToEntityAttribute(String value) {
+          if(value == null){
+           return null;
+        }
+       return Stream.of(Category.values())
+       .filter(c-> c.getValue().equals(value))
+       .findFirst()
+       .orElseThrow();
     }
     
 }
--- crud-spring/src/main/java/com/honorato/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/honorato/crudspring/model/Course.java
@@ -6,8 +6,10 @@ import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.honorato.crudspring.enums.Category;
+import com.honorato.crudspring.enums.converters.CategoryConverter;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -42,8 +44,8 @@ public class Course {
     @NotNull
     //@Length(max = 10)
     //@Pattern(regexp = "back-end|front-end")
-    @Column( nullable = false)
-    @Enumerated(EnumType.STRING)
+    @Column(length = 10, nullable = false)
+    @Convert( converter = CategoryConverter.class)
     private Category category;
 
     @NotNull
